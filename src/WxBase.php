29148ba30bf8f60ebb0d29a4c5b0c4c86f2e9122
山南 <zhangpeichaook@163.com>
2018-02-27 17:15:04 +0800

    Fix the namespace of Exception

--- src/WxBase.php
+++ src/WxBase.php
@@ -9,13 +9,13 @@ class WxBase {
 
 	public function __construct($app_id, $app_secret) {
 		if (empty($app_id) || empty($app_secret)) {
-			throw new InvalidArgumentException('appid/appsecret均不能为空');
+			throw new \InvalidArgumentException('appid/appsecret均不能为空');
 		}
 		if (is_string($app_id) && is_string($app_secret)) {
 			$this->_app_id = $app_id;
 			$this->_app_secret = $app_secret;
 		} else {
-			throw new InvalidArgumentException('appid/appsecret均为字符串');
+			throw new \InvalidArgumentException('appid/appsecret均为字符串');
 		}
 	}
 
